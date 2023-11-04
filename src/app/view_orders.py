2a4def2524d7878240c05c0da147ea8b48c498ae
JRodriguezS <josuesonro@gmail.com>
2023-11-04 00:00:36 -0600

    View Order casi funcional

--- src/app/view_orders.py
+++ src/app/view_orders.py
@@ -9,7 +9,6 @@ class OrderPaymentWindow(QWidget):
 
         # Inicializamos el modelo del restaurante
         self.restaurant_model = restaurant_model
-        
         # Inicializamos las mesas, los logs, platisllos y el inventario
         self.tablesData = restaurant_model.tables
         self.order_log = restaurant_model.order_log
@@ -18,9 +17,12 @@ class OrderPaymentWindow(QWidget):
         self.menu_saludablesData = restaurant_model.menu_saludables
         self.inventoryData = restaurant_model.inventory
 
+        # Inicializar lista en la que se van a guardar los platillos de una orden en construcción
+        self.order_plates = []
+
         # Inicializamos la ventana
         self.setWindowTitle("Ordenes y Pagos")
-        self.setGeometry(100, 100, 1200, 1000)
+        self.setGeometry(100, 100, 1700, 1000)
 
         # Registro de ordenes
         self.title_label = QLabel("Registro de Ordenes", self)
@@ -69,50 +71,296 @@ class OrderPaymentWindow(QWidget):
         self.number_of_customers_text = QLineEdit(self)
         self.number_of_customers_text.setGeometry(180, 230, 100, 20)
 
+        self.payment_method_label = QLabel("Método de Pago:", self)
+        self.payment_method_label.setGeometry(20, 270, 150, 20)
+        self.payment_method_combo = QComboBox(self)
+        self.payment_method_combo.setGeometry(180, 270, 100, 20)
+        self.payment_method_combo.addItem("Efectivo")
+        self.payment_method_combo.addItem("No Efectivo")
+
         # Botones para agregar platos a la orden
         self.combo_button = QPushButton("Agregar Combo", self)
-        self.combo_button.setGeometry(20, 270, 150, 30)
-        #self.combo_button.clicked.connect(self.add_combo)
+        self.combo_button.setGeometry(20, 320, 150, 30)
+        self.combo_button.clicked.connect(self.add_combo)
 
         self.saludable_button = QPushButton("Agregar Menu Saludable", self)
-        self.saludable_button.setGeometry(190, 270, 150, 30)
-        #self.saludable_button.clicked.connect(self.add_saludable)
+        self.saludable_button.setGeometry(190, 320, 150, 30)
+        self.saludable_button.clicked.connect(self.add_saludable)
+
+        #Boton para eliminar platos de la orden
+        self.delete_button = QPushButton("Eliminar Plato", self)
+        self.delete_button.setGeometry(360, 320, 150, 30)
+        self.delete_button.clicked.connect(self.delete_plate)
 
         # Tabla de platillos de la orden
         self.order_label = QLabel("Orden:", self)
-        
+        self.order_label.setGeometry(20, 400, 150, 20)
+        self.order_table = QTableWidget(self)
+        self.order_table.setGeometry(20, 370, 750, 200)
+        self.order_table.setColumnCount(7)
+        self.order_table.setHorizontalHeaderLabels(["Nombre", "Precio", "Calorias", "Bebida", "Proteina", "Acompañamiento", "Postre"])
+        self.order_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.order_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
-    """
-        self.order_label = QLabel("Orden:", self)
-        self.order_label.setGeometry(20, 220, 150, 20)
-        # Obtener los combos para agregarlos al menu de ordenes
-        self.menu_combo_items = []
-        for combo_plate in self.combo_platesData:
-            self.menu_combo_items.append(combo_plate.name + " " + combo_plate.drink.name + " " + combo_plate.protein.name + " " + combo_plate.side_dish.name + " " + combo_plate.dessert.name + " " + str(combo_plate.price) + " " + str(combo_plate.calories))
-        self.menu_combo_checkboxes = []
-        for i in range(len(self.menu_combo_items)):
-            checkbox = QCheckBox(self.menu_combo_items[i], self)
-            checkbox.setGeometry(20, 240 + i * 20, 750, 20)
-            self.menu_combo_checkboxes.append(checkbox)
-        # Obtener los platos saludables al menu de ordenes
-        self.menu_saludable_items = []
-        for menu_saludable in self.menu_saludablesData:
-            self.menu_saludable_items.append(menu_saludable.name + " " + str(menu_saludable.price) + " " + str(menu_saludable.calories))
-        self.menu_saludable_checkboxes = []
-        for i in range(len(self.menu_saludable_items)):
-            checkbox = QCheckBox(self.menu_saludable_items[i], self)
-            checkbox.setGeometry(20, 240 + (i + len(self.menu_combo_items)) * 20, 750, 20)
-            self.menu_saludable_checkboxes.append(checkbox)
-
-        # Cerrar orden
+        # boton cerrar orden
         self.close_order_button = QPushButton("Cerrar Orden", self)
-        self.close_order_button.setGeometry(20, 250, 150, 30)
+        self.close_order_button.setGeometry(20, 590, 150, 30)
         self.close_order_button.clicked.connect(self.close_order)
 
+        # Tabla que muestra ordenes existentes
+        self.order_log_label = QLabel("Ordenes Existentes:", self)
+        self.order_log_label.setGeometry(20, 630, 150, 20)
+        self.order_log_table = QTableWidget(self)
+        self.order_log_table.setGeometry(20, 650, 750, 200)
+        self.order_log_table.setColumnCount(5)
+        self.order_log_table.setHorizontalHeaderLabels(["Fecha", "Numero de Mesa", "Nombre del Cliente", "Estado", "Platos"])
+        self.order_log_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.order_log_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.populate_order_log_table()
+        
+        # Tabla de combos a elegir
+        self.combo_label = QLabel("Combos:", self)
+        self.combo_label.setGeometry(800, 20, 150, 20)
+        self.combo_table = QTableWidget(self)
+        self.combo_table.setGeometry(800, 40, 750, 200)
+        self.combo_table.setColumnCount(7)
+        self.combo_table.setHorizontalHeaderLabels(["Nombre", "Precio", "Calorias", "Bebida", "Proteina", "Acompañamiento", "Postre"])
+        self.combo_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.combo_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.populate_combo_table_no_id()
 
-    def close_order(self):
-        # Aquí debes implementar la lógica para cerrar la orden.
-        # Esto incluiría registrar los platos seleccionados, detalles de la mesa, etc.
-        # Una vez que se cierra la orden, ya no se pueden realizar modificaciones.
-        pass
+        # Tabla de menu saludables a elegir
+        self.saludable_label = QLabel("Menus Saludables:", self)
+        self.saludable_label.setGeometry(800, 270, 150, 20)
+        self.saludable_table = QTableWidget(self)
+        self.saludable_table.setGeometry(800, 290, 750, 200)
+        self.saludable_table.setColumnCount(7)
+        self.saludable_table.setHorizontalHeaderLabels(["Nombre", "Precio", "Calorias", "Bebida", "Proteina", "Acompañamiento", "Postre"])
+        self.saludable_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.saludable_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.populate_saludable_table_no_id()
+
+        # Boton para generar nuevos menus saludables
+        self.generate_saludable_button = QPushButton("Generar Menus Saludables", self)
+        self.generate_saludable_button.setGeometry(800, 500, 350, 30)
+        #self.generate_saludable_button.clicked.connect(self.generate_saludable)
+        
+        # Tabla de los pagos
+        self.payment_log_label = QLabel("Pagos:", self)
+        self.payment_log_label.setGeometry(800, 630, 150, 20)
+        self.payment_log_table = QTableWidget(self)
+        self.payment_log_table.setGeometry(800, 650, 750, 200)
+        self.payment_log_table.setColumnCount(4)
+        self.payment_log_table.setHorizontalHeaderLabels(["Orden", "Es efectivo", "Monto", "Estado"])
+        self.payment_log_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.payment_log_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.populate_payment_log_table()
+
+    # Metodo para crear una orden
     """
+    Se extraen los datos de la interfaz gráfica y se crea un objeto Order.
+    Se debe de validar que los datos sean correctos.
+    """
+    def create_order(self) -> None:
+        # Obtener el número de mesa
+        try:
+            table_number = int(self.table_input.text())
+        except:
+            QMessageBox.warning(self, "Error", "El número de mesa debe ser un número entero", QMessageBox.Ok)
+            return
+        
+        # Obtener el nombre del cliente
+        try:
+            customer_name = str(self.customer_name_input.text())
+        except:
+            QMessageBox.warning(self, "Error", "El nombre del cliente debe ser un texto", QMessageBox.Ok)
+            return
+        
+        # Obtener platillos de la orden
+        if len(self.order_plates) == 0:
+            QMessageBox.warning(self, "Error", "La orden debe tener al menos un platillo", QMessageBox.Ok)
+            return
+        else:
+            order_plates = self.order_plates
+        
+        # Crear el objeto Order
+        order = Order(table_number=table_number, customers_name=customer_name, status="Lista")
+
+        # Agregar los platillos a la orden
+        for plate in order_plates:
+            order.add_dish(plate)
+
+        # Se retorna el objeto Order
+        return order
+
+    def close_order(self) -> None:
+        """ 
+        Esto incluiría registrar los platos seleccionados, detalles de la mesa, etc.
+        Una vez que se cierra la orden, ya no se pueden realizar modificaciones.
+        Al cerrar una orden, se debe de crear un objeto Order y agregarlo al log de ordenes.
+        Se deben generar los pagos correspondientes a la orden.
+        """
+        #region Crear objeto Order
+        order = self.create_order()
+        if order is None:
+            return
+        #endregion
+
+        # Se agrega la orden a una lista para poder agregarla al pago
+        orders = []
+        orders.append(order)
+
+        #region Crear objeto Payment
+        if self.single_payment.isChecked():
+            is_cash = True if self.payment_method_combo.currentText() == "Efectivo" else False
+            payment = Payment(orders=orders, is_cash=is_cash)
+            print(payment.billing_amount)
+        else:
+            #Es necesario generar un pago por cada cliente y plato
+            #Primero validamos que el número de clientes y el número de platos sea el mismo
+            if len(orders[0].dishes) != int(self.number_of_customers_text.text()):
+                QMessageBox.warning(self, "Error", "El número de clientes debe ser igual al número de platos", QMessageBox.Ok)
+                return
+            else:
+                # Se crea un pago por cada cliente y plato
+                payments = []
+                for i in range(len(orders[0].dishes)):
+                    orders_temp = []
+                    orders_temp.append(orders[0])
+                    payment = Payment(orders=orders_temp, is_cash=True)
+                    payments.append(payment)
+        #endregion
+
+        #region de actualizar la mesa
+        # Se obtiene la mesa
+        table = self.tablesData[order.table_number]
+        # Se actualiza el order_log de la mesa
+        table.add_order(order)
+        self.populate_order_log_table()
+        # Se actualiza el payment_log de la mesa
+        table.add_payment(payment)
+        self.populate_payment_log_table()
+        # Se actualiza el numero de clientes de la mesa
+        table.edit_number_of_customers(int(self.number_of_customers_text.text()))
+        # Se actualiza si es single_bill o no
+        table.edit_single_bill(self.single_payment.isChecked())
+        # Se actualiza el status de la mesa
+        table.edit_status("Ocupada")
+        #endregion
+
+        # Se devuelve la mesa actualizada a la lista de mesas
+        self.tablesData[order.table_number] = table
+
+    def add_combo(self) -> None:
+        # usar try except para evitar que se caiga el programa si no se selecciona una fila
+        try:
+            # Obtener nombre del combo seleccionado
+            selected_combo_name = self.combo_table.selectedItems()[0].text()
+            # Buscar el combo seleccionado en la lista de combos
+            selected_combo = next(combo for combo in self.combo_platesData if combo.name == selected_combo_name)
+            # Actualizar la lista de platos de la orden
+            self.order_plates.append(selected_combo)
+
+            self.populate_order_table()
+        except:
+            QMessageBox.warning(self, "Error", "No se ha seleccionado un combo", QMessageBox.Ok)
+
+    def add_saludable(self) -> None:
+        try:
+            selected_saludable_name = self.saludable_table.selectedItems()[0].text()
+            selected_saludable = next(saludable for saludable in self.menu_saludablesData if saludable.name == selected_saludable_name)
+            self.order_plates.append(selected_saludable)
+            self.populate_order_table()
+        except:
+            QMessageBox.warning(self, "Error", "No se ha seleccionado un menu saludable", QMessageBox.Ok)
+
+    def delete_plate(self) -> None:
+        try:
+            selected_plate_name = self.order_table.selectedItems()[0].text()
+            selected_plate = next(plate for plate in self.order_plates if plate.name == selected_plate_name)
+            self.order_plates.remove(selected_plate)
+            self.populate_order_table()
+        except:
+            QMessageBox.warning(self, "Error", "No se ha seleccionado un plato", QMessageBox.Ok)
+
+    def populate_order_table(self) -> None:
+        # Se puebla la tabla de ordenes con los platos de la orden
+        self.order_table.setRowCount(len(self.order_plates))
+        
+        for i, plate in enumerate(self.order_plates):
+            self.order_table.setItem(i, 0, QTableWidgetItem(plate.name))
+            self.order_table.setItem(i, 1, QTableWidgetItem(str(plate.price)))
+            self.order_table.setItem(i, 2, QTableWidgetItem(str(plate.calories)))
+            self.order_table.setItem(i, 3, QTableWidgetItem(plate.drink.name))
+            self.order_table.setItem(i, 4, QTableWidgetItem(plate.protein.name))
+            self.order_table.setItem(i, 5, QTableWidgetItem(plate.side_dish.name))
+            self.order_table.setItem(i, 6, QTableWidgetItem(plate.dessert.name))
+
+    def populate_combo_table_no_id(self) -> None:
+
+        combo_plates = self.combo_platesData
+        
+        self.combo_table.setRowCount(len(combo_plates))
+
+        for i, combo_plate in enumerate(combo_plates):
+            self.combo_table.setItem(i, 0, QTableWidgetItem(combo_plate.name))
+            self.combo_table.setItem(i, 1, QTableWidgetItem(str(combo_plate.price)))
+            self.combo_table.setItem(i, 2, QTableWidgetItem(str(combo_plate.calories)))
+            self.combo_table.setItem(i, 3, QTableWidgetItem(combo_plate.drink.name))
+            self.combo_table.setItem(i, 4, QTableWidgetItem(combo_plate.protein.name))
+            self.combo_table.setItem(i, 5, QTableWidgetItem(combo_plate.side_dish.name))
+            self.combo_table.setItem(i, 6, QTableWidgetItem(combo_plate.dessert.name))
+
+    def populate_saludable_table_no_id(self) -> None:
+
+        menu_saludables = self.menu_saludablesData
+        
+        self.saludable_table.setRowCount(len(menu_saludables))
+
+        for i, menu_saludable in enumerate(menu_saludables):
+            self.saludable_table.setItem(i, 0, QTableWidgetItem(menu_saludable.name))
+            self.saludable_table.setItem(i, 1, QTableWidgetItem(str(menu_saludable.price)))
+            self.saludable_table.setItem(i, 2, QTableWidgetItem(str(menu_saludable.calories)))
+            self.saludable_table.setItem(i, 3, QTableWidgetItem(menu_saludable.drink.name))
+            self.saludable_table.setItem(i, 4, QTableWidgetItem(menu_saludable.protein.name))
+            self.saludable_table.setItem(i, 5, QTableWidgetItem(menu_saludable.side_dish.name))
+            self.saludable_table.setItem(i, 6, QTableWidgetItem(menu_saludable.dessert.name))
+
+    def populate_order_log_table(self) -> None:
+            # Limpiar la tabla existente
+        self.order_log_table.setRowCount(0)
+        
+        # Recorrer el historial de órdenes y agregar las órdenes a la tabla
+        for date, orders in self.order_log.order_history.items():
+            for order in orders:
+                # Obtener detalles de la orden en una cadena
+                order_details = ""
+                for dish in order.dishes:
+                    order_details += f"{dish.name}, "
+                order_details = order_details.rstrip(", ")
+                
+                # Agregar la orden a la tabla
+                row_position = self.order_log_table.rowCount()
+                self.order_log_table.insertRow(row_position)
+                self.order_log_table.setItem(row_position, 0, QTableWidgetItem(date.strftime("%Y-%m-%d")))
+                self.order_log_table.setItem(row_position, 1, QTableWidgetItem(str(order.table_number)))
+                self.order_log_table.setItem(row_position, 2, QTableWidgetItem(order.customers_name))
+                self.order_log_table.setItem(row_position, 3, QTableWidgetItem(order.status))
+                self.order_log_table.setItem(row_position, 4, QTableWidgetItem(order_details))
+
+    def populate_payment_log_table(self) -> None:   
+
+       # Limpiar la tabla existente
+        self.payment_log_table.setRowCount(0)
+
+        # Recorrer el historial de pagos y agregar los pagos a la tabla
+        for date, payments in self.payment_log.payment_history.items():
+            for payment in payments:
+                # Obtener detalles del pago en una cadena
+                orders_info = ", ".join([f"{order.table_number} ({order.customers_name})" for order in payment.orders])
+                payment_details = f"Órdenes: {orders_info}, Método: {'Efectivo' if payment.is_cash else 'No Efectivo'}, Monto: {payment.billing_amount}, Estado: {'Exitoso' if payment.status else 'No Exitoso'}"
+
+                # Agregar el pago a la tabla
+                row_position = self.payment_log_table.rowCount()
+                self.payment_log_table.insertRow(row_position)
+                self.payment_log_table.setItem(row_position, 0, QTableWidgetItem(date.strftime("%Y-%m-%d")))
